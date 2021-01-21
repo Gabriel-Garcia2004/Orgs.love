a3c058fbb6803ee3ced12c13dd34263bc15226a5
Gabriel-Garcia2004 <garcia.gabrielrs@gmail.com>
2021-01-21 15:25:56 -0300

    Página acess completa e responsiva

--- public/script/ongs-loves.js
+++ public/script/ongs-loves.js
@@ -1,9 +1,9 @@
-const map = L.map("mapid").setView([-22.8365692, -42.3644214], 15);
+const map = L.map("mapid").setView([-15.7666161, -48.2669312], 15);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
+/*L.marker([51.5, -0.09]).addTo(map)
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+    .openPopup();*/
